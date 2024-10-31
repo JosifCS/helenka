68d99f7a3df3d60f2ce40faae935973da42cf326
JosifCS <josef.holat@ceskysoftware.cz>
2024-10-31 19:28:51 +0100

    feat: button sizes

--- components/basic/button.tsx
+++ components/basic/button.tsx
@@ -10,17 +10,18 @@ export const buttonVariants = cva(String.Empty, {
   variants: {
     variant: {
       primary:
-        "rounded-full border border-solid transition-colors flex items-center justify-center text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 border-transparent bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc]",
+        "rounded-full border border-solid transition-colors flex items-center justify-center border-transparent bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc]",
       secondary:
-        "rounded-full border border-solid transition-colors flex items-center justify-center text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 border-black/[.08] dark:border-white/[.145] hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent sm:min-w-44",
+        "rounded-full border border-solid transition-colors flex items-center justify-center border-black/[.08] dark:border-white/[.145] hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent sm:min-w-44",
       link: "gap-2 hover:underline hover:underline-offset-4",
     },
     size: {
-      sm: "h-8 rounded-md px-3 text-xs",
-      md: "h-9 px-4 py-2",
-      lg: "h-10 rounded-md px-8",
+      sm: "text-xs sm:text-sm h-8 sm:h-10 px-3 sm:px-4",
+      md: "text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5",
+      lg: "text-base sm:text-lg h-12 sm:h-14 px-5 sm:px-6",
     },
   },
+  compoundVariants: [],
   defaultVariants: {
     size: "md",
     variant: "primary",
