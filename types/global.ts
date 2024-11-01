75ea32b1c410eb283bf21ff38a26647294144bc1
JosifCS <josef.holat@ceskysoftware.cz>
2024-11-01 22:11:28 +0100

    add type RawTableDefinition

--- types/global.ts
+++ types/global.ts
@@ -1,1 +1,11 @@
 export type DateRange = { from: Date; to: Date };
+
+export type RawTableDefinition = {
+  value: string;
+  columns: {
+    key: string;
+    type: string;
+    column: number;
+    value: string;
+  }[];
+};
