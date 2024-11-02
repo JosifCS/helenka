628304ddc57d6482082b817f55e0a3d2379a5973
JosifCS <josef.holat@ceskysoftware.cz>
2024-11-02 20:45:38 +0100

    add new import btn

--- app/reports/page.tsx
+++ app/reports/page.tsx
@@ -1,11 +1,18 @@
 import { PageTitle } from "@/components/page-title"
+import { Button } from "@/components/ui/button"
 import { getTranslations } from "next-intl/server"
+import { CirclePlus } from "lucide-react"
+import Link from "next/link"
 
 export default async function Page() {
 	const t = await getTranslations("Reports")
 	return (
 		<main>
-			<PageTitle title={t("title")} description={t("description")} />
+			<PageTitle title={t("title")} description={t("description")}>
+				<Link href="/reports/import" passHref>
+					<Button icon={CirclePlus}>{t("newImport")}</Button>
+				</Link>
+			</PageTitle>
 			<div className=""></div>
 		</main>
 	)
