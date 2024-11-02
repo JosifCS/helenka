168ececf7f3db25d329652c01da47f5042eecb13
JosifCS <josef.holat@ceskysoftware.cz>
2024-11-02 17:59:27 +0100

    settings: tab size 4

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,35 +10,35 @@ String.LF = "\n";
 String.CRLF = "\r\n";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+	src: "./fonts/GeistVF.woff",
+	variable: "--font-geist-sans",
+	weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+	src: "./fonts/GeistMonoVF.woff",
+	variable: "--font-geist-mono",
+	weight: "100 900",
 });
 
 export const metadata: Metadata = {
-  title: "Helenka",
-  description:
-    "Hospodářská evidence likvidity, evidence nákladů a kontrola aktiv",
+	title: "Helenka",
+	description:
+		"Hospodářská evidence likvidity, evidence nákladů a kontrola aktiv",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <TopMenu />
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+			>
+				<TopMenu />
+				{children}
+			</body>
+		</html>
+	);
 }
--- components/top-menu.tsx
+++ components/top-menu.tsx
@@ -1,23 +1,23 @@
 import {
-  Menubar,
-  MenubarContent,
-  MenubarItem,
-  MenubarMenu,
-  MenubarSeparator,
-  MenubarTrigger,
+	Menubar,
+	MenubarContent,
+	MenubarItem,
+	MenubarMenu,
+	MenubarSeparator,
+	MenubarTrigger,
 } from "./ui/menubar";
 
 export function TopMenu() {
-  return (
-    <Menubar className="rounded-none border-b border-none px-2 lg:px-4">
-      <MenubarMenu>
-        <MenubarTrigger className="font-bold">Helenka</MenubarTrigger>
-        <MenubarContent>
-          <MenubarItem>O Helence</MenubarItem>
-          <MenubarSeparator />
-          <MenubarItem>Nastavení</MenubarItem>
-        </MenubarContent>
-      </MenubarMenu>
-    </Menubar>
-  );
+	return (
+		<Menubar className="rounded-none border-b border-none px-2 lg:px-4">
+			<MenubarMenu>
+				<MenubarTrigger className="font-bold">Helenka</MenubarTrigger>
+				<MenubarContent>
+					<MenubarItem>O Helence</MenubarItem>
+					<MenubarSeparator />
+					<MenubarItem>Nastavení</MenubarItem>
+				</MenubarContent>
+			</MenubarMenu>
+		</Menubar>
+	);
 }
