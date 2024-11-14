6b113667251e81190361a47368d25d444e4ad08c
JosifCS <109961607+JosifCS@users.noreply.github.com>
2024-11-14 21:28:00 +0100

    money function with fractionDigits param

--- modules/format-utils.ts
+++ modules/format-utils.ts
@@ -6,16 +6,19 @@ import { createFormatter } from "next-intl"
  * @param number Next.intl `number` function from `useFormatter`.
  * @param value Number value.
  * @param currency ISO 4217 currency code.
- * @returns
+ * @param fractionDigits Number of fraction digits. Default value is *2*.
+ * @returns Number formatted as money value.
  */
 export function money(
 	number: ReturnType<typeof createFormatter>["number"],
 	value: number,
-	currency: CurrencyCode
+	currency: CurrencyCode,
+	fractionDigits: number = 2
 ) {
 	return number(value, {
+		style: "currency",
 		currency,
-		maximumFractionDigits: 4,
-		minimumFractionDigits: 4,
+		maximumFractionDigits: fractionDigits,
+		minimumFractionDigits: fractionDigits,
 	})
 }
