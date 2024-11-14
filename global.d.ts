87d7ae75e55fbacabde22c7a65e8e6ac53a2a4ca
JosifCS <109961607+JosifCS@users.noreply.github.com>
2024-11-14 20:15:19 +0100

    next-intl type validation

--- global.d.ts
+++ global.d.ts
@@ -1,10 +1,19 @@
-interface StringConstructor {
-  /** Ekvivalent prázdného stringu *""*. */
-  Empty: string;
-  /** *\r* */
-  CR: string;
-  /** *\n* */
-  LF: string;
-  /** *r\n* */
-  CRLF: string;
+import en from "./messages/en.json"
+
+type Messages = typeof en
+
+declare global {
+	// Use type safe message keys with `next-intl`
+	interface IntlMessages extends Messages {}
+
+	interface StringConstructor {
+		/** Ekvivalent prázdného stringu *""*. */
+		Empty: string
+		/** *\r* */
+		CR: string
+		/** *\n* */
+		LF: string
+		/** *r\n* */
+		CRLF: string
+	}
 }
